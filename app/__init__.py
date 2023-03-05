7d7248ed41dbe323e15f68607d5a1794d6345d03
root <DiaA6383@gmail.com>
2023-03-05 16:30:22 +0000

    committing database-edit __init__

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
-db = MySQLDatabase(os.getenv("MYSQL_DATABASE"), host=os.getenv("MYSQL_HOST"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"))
+mydb =
+ MySQLDatabase(os.getenv("MYSQL_DATABASE"), 
+	user=os.getenv("MYSQL_USER"),
+	password=os.getenv("MYSQL_PASSWORD"),
+	host=os.getenv("MYSQL_HOST"),
+	port=3306
+)
+
+print(mydb)
 
 @app.route('/')
 def index():
