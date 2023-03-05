0d599775e12f7e14d70d96d55b0666e74235e09e
root <DiaA6383@gmail.com>
2023-03-05 16:00:31 +0000

    committing database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
+from peewee import *
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
-
+db = MySQLDatabase(os.getenv("MYSQL_DATABASE"), host=os.getenv("MYSQL_HOST"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"))
 
 @app.route('/')
 def index():
