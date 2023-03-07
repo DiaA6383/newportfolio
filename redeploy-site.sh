86475b066f929b31d507f9b56a16acd28037b41e
root <DiaA6383@gmail.com>
2023-03-07 23:19:45 +0000

    redeploy change

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-# Kill all existing tmux sessions
-tmux kill-server
-
 # Change to your project directory
 cd /root/newportfolio
 
 # Fetch latest changes from main branch and reset to latest commit
 git fetch && git reset origin/main --hard
 
-# Activate the Python virtual environment
+# Activate the Python virtual environment and install dependencies
 source /root/newportfolio/env/bin/activate
-# Upgrade pip to the latest version
-pip install --upgrade pip
-# Install Python dependencies using pip
 pip install -r requirements.txt
 
-# Start a new tmux session and run the Flask server
-tmux new-session -d -s myflaskserver 'cd /root/newportfolio && source /root/newportfolio/env/bin/activate && flask run --host=0.0.0.0'
+# Restart myportfolio service
+systemctl restart myportfolio
 
-echo "Flask server started in a new tmux session"
-#clone repo to root and ssh 
+echo "Site redeployed and myportfolio service restarted"
+
