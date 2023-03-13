a70e4a03ae1326c2b9bcd907c49fbea89c733b4a
root <DiaA6383@gmail.com>
2023-03-13 01:17:45 +0000

    added comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 from peewee import *
 from datetime import datetime
 import os
@@ -5,11 +6,16 @@ from playhouse.shortcuts import model_to_dict
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
-app = Flask(__name__)
-mydb =  MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
 
-#define a model for the timeline
+# Create Flask application instance
+app = Flask(__name__)
+
+# Initialize a MySQL database instance using environment variables
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
+
+# Define a model for the timeline using Peewee ORM
 class TimelinePost(Model):
     name = CharField()
     email = CharField()
@@ -18,17 +24,18 @@ class TimelinePost(Model):
 
     class Meta:
         database = mydb
+
+# Connect to the database if this script is being run as the main program
 if __name__ == "__main__":
     mydb.initialize()
     mydb.connect
 
-#mydb.connect()
-#mydb.create_tables([TimelinePost])
-
+# Define a route for the home page
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
+# Define a route for posting a new timeline post
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
     name = request.form['name']
@@ -37,12 +44,15 @@ def post_time_line_post():
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
     return model_to_dict(timeline_post)
 
+# Define a route for retrieving all timeline posts
 @app.route('/api/timeline_post', methods=['GET'])
 def get_time_line_post():
     return {
         'timeline_posts': [model_to_dict(p) for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())]
     }
-    #this is just how it was put
+
+# Define a route for displaying the timeline posts
 @app.route('/timeline')
 def timeline_post():
-    return render_template('timeline.html', title = "Timeline")
+    return render_template('timeline.html', title="Timeline")
+~
