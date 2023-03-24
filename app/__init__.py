fbb22749e526838642e580375f83fef69c84ce49
Alex <DiaA6383@gmail.com>
2023-03-23 20:28:38 -0400

    creat a docker file

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,13 @@ load_dotenv()
 app = Flask(__name__)
 
 # Initialize a MySQL database instance using environment variables
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
+mydb = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306,
+)
 
 # Define a model for the timeline using Peewee ORM
 class TimelinePost(Model):
@@ -26,20 +32,22 @@ class TimelinePost(Model):
     class Meta:
         database = mydb
 
+
 # Connect to the database if this script is being run as the main program
 if __name__ == "__main__":
     initialize_app()
 # Define a route for the home page
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template("index.html", title="MLH Fellow", url=os.getenv("URL"))
+
 
 # Define a route for posting a new timeline post
-@app.route('/api/timeline_post', methods=['POST'])
+@app.route("/api/timeline_post", methods=["POST"])
 def post_time_line_post():
-    name = request.form.get('name', '').strip()
-    email = request.form.get('email', '').strip()
-    content = request.form.get('content', '').strip()
+    name = request.form.get("name", "").strip()
+    email = request.form.get("email", "").strip()
+    content = request.form.get("content", "").strip()
 
     if not name:
         return "Invalid name", 400
@@ -47,46 +55,55 @@ def post_time_line_post():
     if not content:
         return "Invalid content", 400
 
-    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    email_regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     if not re.match(email_regex, email):
         return "Invalid email", 400
 
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
+    name = request.form["name"]
+    email = request.form["email"]
+    content = request.form["content"]
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
     return model_to_dict(timeline_post)
 
+
 # Define a route for retrieving all timeline posts
-@app.route('/api/timeline_post', methods=['GET'])
+@app.route("/api/timeline_post", methods=["GET"])
 def get_time_line_post():
     return {
-        'timeline_posts': [model_to_dict(p) for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())]
+        "timeline_posts": [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
     }
 
+
 # Define a route for displaying the timeline posts
-@app.route('/timeline')
+@app.route("/timeline")
 def timeline_post():
-    return render_template('timeline.html', title="Timeline")
+    return render_template("timeline.html", title="Timeline")
+
 
 def initialize_database():
     if os.getenv("TESTING") == "true":
         print("Running in test mode")
-        test_db = SqliteDatabase(':memory:')
+        test_db = SqliteDatabase(":memory:")
         test_db.bind([TimelinePost], bind_refs=False, bind_backrefs=False)
         test_db.connect()
         test_db.create_tables([TimelinePost], safe=True)
     else:
-        mydb.init(os.getenv("MYSQL_DATABASE"),
-        user=os.getenv("MYSQL_USER"),
-        password=os.getenv("MYSQL_PASSWORD"),
-        host=os.getenv("MYSQL_HOST"),
-        port=3306
+        mydb.init(
+            os.getenv("MYSQL_DATABASE"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            host=os.getenv("MYSQL_HOST"),
+            port=3306,
         )
 
         # Connect to the database and create the tables if they don't exist
         mydb.connect()
         mydb.create_tables([TimelinePost], safe=True)
+
+
 def initialize_app():
     initialize_database()
-    app.run
+    app.run(host="0.0.0.0", port=5000)
--- curl-test.sh
+++ curl-test.sh
@@ -6,6 +6,11 @@ content=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
 # Send a POST request to create a new timeline post
 curl -d "name=John&email=john@example.com&content=$content" -X POST http://localhost:5000/api/timeline_post
 
+curl http://localhost:5000/api/timeline_post
+{"timeline_posts":[]}
+
+curl -X POST http://localhost:5000/api/timeline_post -d 'name=Wei&email=wei.he@mlh.io&content=Just learned how to run a docker container!' '{"content":"Just Learned how to run a docker container!","created_at":"Fri, 20 May 2022 04:57:35 GMT","email":"wei.he@mlh.io","id":1,"name":"Wei"}'
+
 # Send a GET request to retrieve all timeline posts and check if the random content string is in the response
 if curl -s http://localhost:5000/api/timeline_post | grep -q "$content"; then
     echo "Timeline post successfully added"
