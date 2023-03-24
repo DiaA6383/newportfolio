55d1e9677fc82cef4f71bb13852e1cfa52e9ff57
Alex <DiaA6383@gmail.com>
2023-03-24 14:49:02 -0400

    back to normal

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,8 @@
 # in this case, the 'activate' script for the virtual environment.
 source /Users/alejandrodiaz/newportfolio/env/bin/activate
 
+pip install -r requirements.txt
+#install the requirements.txt file
 # Run the unittest discover command to find and run all tests in the 'tests' directory.
 # The '-v' flag enables verbose output, providing more detailed information about the tests.
 python -m unittest discover -v tests/
